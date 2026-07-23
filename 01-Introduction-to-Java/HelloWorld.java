d6a1b0f2076e11225e0dc8cfb2832ddfa16cb75a
Didipsha Guha <didipshaguha0620@gmail.com>
2026-07-23 18:03:27 +0530

    Add HelloWorld.java with basic Java concepts

---
+++ 01-Introduction-to-Java/HelloWorld.java
@@ -0,0 +1,36 @@
+/*
+ * Concepts Covered:
+ * 1. Class
+ * 2. main() Method
+ * 3. Comments
+ * 4. Output Statements
+ * 5. Program Execution Flow
+ */
+
+public class HelloWorld {
+
+    /*
+     * main() is the entry point of a Java application.
+     *
+     * Analogy:
+     * Think of a house.
+     * The class is the house,
+     * and main() is the front door.
+     *
+     * Whenever the program starts,
+     * Java enters through this door first.
+     */
+    public static void main(String[] args) {
+
+        /*
+         * System.out.println()
+         *
+         * System  -> Java's built-in system utilities
+         * out     -> Standard output stream (console)
+         * println -> Print a line and move to the next line
+         */
+
+        System.out.println("Hello, World!");
+
+    }
+}
